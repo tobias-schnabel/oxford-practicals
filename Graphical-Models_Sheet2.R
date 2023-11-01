ed8029880e6e3daf1cdab455f0e1b25d3368a85d
Tobias Schnabel <ctobiasschnabel@gmail.com>
2023-11-01 10:55:40 +0000

    worked on R part of sheet 2 of graphical models

--- Graphical-Models_Sheet2.R
+++ Graphical-Models_Sheet2.R
@@ -7,3 +7,21 @@ solve(cov(marks))
 # from lectures in which ‘analysis’ and ‘statistics’ are independent 
 # of ‘mechanics’ and ‘vectors’ conditional on ‘algebra’.
 
+colnames(marks)
+# [1] "mechanics"  "vectors"    "algebra"    "analysis"  
+# [5] "statistics"
+
+# so 4,5 indep of 1,2 given 3
+
+sigma = cov(marks)
+K <- solve(cov(marks))
+K_hat <- matrix(0, 5, 5)
+K_hat[1:2, 1:2] <- K[1:2, 1:2]
+K_hat[3,3] <- K[3,3]
+K_hat[3, 3] <- K[3,3] - (1 / sigma[3, 3])
+K_hat[4:5, 4:5] = K[4:5, 4:5]
+
+rownames(K_hat) <- rownames(K)
+colnames(K_hat) <- colnames(K)
+round(K_hat, 3)
+round(K, 3)
