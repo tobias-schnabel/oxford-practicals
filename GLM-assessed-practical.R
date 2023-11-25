583badb3dafedbe3732e4d1b58bf34e2651b47fa
Tobias Schnabel <ctobiasschnabel@gmail.com>
2023-11-25 07:03:35 +0000

    set up file for assessed practical on GLMs for applied statistics

---
+++ GLM-assessed-practical.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(ggplot2)
+library(gridExtra)
+library(patchwork) # to combine plots
+library(stargazer)
+library(kableExtra)
+library(sandwich) # for robust Standard Errors
+library(MASS) # for stepwise AIC and BIC selection
+library(ggfortify)
+
+## Set Paths for tables and figures
+root = "/Users/ts/Git/Practicals"
+tab = "/Users/ts/Library/CloudStorage/Dropbox/Apps/Overleaf/GLM Practical/Tables"
+fig = "/Users/ts/Library/CloudStorage/Dropbox/Apps/Overleaf/GLM Practical/Figures"
+code = "/Users/ts/Library/CloudStorage/Dropbox/Apps/Overleaf/GLM Practical/Code"
+
+if (getwd() != root) {
+  setwd(root)
+}
+# Load data
+data <- read.csv("swim.csv")
+
+setwd(code)
+file.copy(from = "/Users/ts/Git/Practicals/GLM-assessed-practical.R",
+          to = "/Users/ts/Library/CloudStorage/Dropbox/Apps/Overleaf/GLM Practical/Code",
+          overwrite = T)
+setwd(root)
