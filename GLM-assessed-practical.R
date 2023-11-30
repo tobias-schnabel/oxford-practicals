dc0ce5e526d92d61ec5602cf154ba0d36643df2a
Tobias Schnabel <ctobiasschnabel@gmail.com>
2023-11-30 14:56:08 +0000

    did EDA

--- GLM-assessed-practical.R
+++ GLM-assessed-practical.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(xtable)
 library(ggplot2)
 library(gridExtra)
 library(patchwork) # to combine plots
@@ -14,14 +15,196 @@ tab = "/Users/ts/Library/CloudStorage/Dropbox/Apps/Overleaf/GLM Practical/Tables
 fig = "/Users/ts/Library/CloudStorage/Dropbox/Apps/Overleaf/GLM Practical/Figures"
 code = "/Users/ts/Library/CloudStorage/Dropbox/Apps/Overleaf/GLM Practical/Code"
 
-if (getwd() != root) {
-  setwd(root)
-}
-# Load data
-data <- read.csv("swim.csv")
-
 setwd(code)
 file.copy(from = "/Users/ts/Git/Practicals/GLM-assessed-practical.R",
           to = "/Users/ts/Library/CloudStorage/Dropbox/Apps/Overleaf/GLM Practical/Code",
           overwrite = T)
 setwd(root)
+if (getwd() != root) {
+  setwd(root)
+}
+# Load data
+data_raw <- read_csv("mortg.csv")
+
+### EDA ###
+# Cast as factors
+data <- data_raw %>%
+  mutate(
+    approved = as.factor(approved),
+    self = as.factor(self),
+    single = as.factor(single),
+    white = as.factor(white),
+    mcs = factor(mcs, levels = 1:4)
+  )
+# Summary stats functions
+summary_stats <- function(x) {
+  c(mean = mean(x, na.rm = TRUE), 
+    median = median(x, na.rm = TRUE),
+    sd = sd(x, na.rm = TRUE),
+    min = min(x, na.rm = TRUE),
+    max = max(x, na.rm = TRUE))
+}
+
+# Split data
+data_num <- data %>% dplyr::select(hir, odir, lvr, uria)
+data_fact <- data_raw %>% dplyr::select(approved, mcs, self, single, white)
+
+# Get sumstats
+# Apply the function to each column
+numerical_summary <- t(sapply(data_num, summary_stats))
+factor_summary <- t(sapply(data_fact, summary_stats))
+sumstats <- rbind(numerical_summary, factor_summary)
+colnames(sumstats) <- c("Mean", "Median", "SD", "Min", "Max")
+
+# Make table
+sumtable <- xtable(sumstats, caption = "Summary Statistics")
+
+## EDA plots
+# plot colors
+plotcolors <- ggthemes::tableau_color_pal()(10)
+# Continuous predictors, bivariate
+plot_hir <- ggplot(data, aes(x = approved, y = hir, fill = approved)) +
+  geom_violin(trim = T) + # Add violin plot
+  geom_boxplot(width = 0.1, fill = "white") + # Add boxplot inside
+  scale_fill_manual(values = plotcolors[1:2]) +
+  theme_minimal() +
+  labs(x = "Approved", y = "Ratio of Housing Expenses to Income") +
+  theme(legend.position = "none")
+
+plot_odir <- ggplot(data, aes(x = approved, y = odir, fill = approved)) +
+  geom_violin(trim = T) + # Add violin plot
+  geom_boxplot(width = 0.1, fill = "white") + # Add boxplot inside
+  scale_fill_manual(values = plotcolors[3:4]) +
+  theme_minimal() +
+  labs(x = "Approved", y = "Ratio of other Debt Payments to Income") +
+  theme(legend.position = "none")
+
+plot_lvr <- ggplot(data, aes(x = approved, y = lvr, fill = approved)) +
+  geom_violin(trim = T) + # Add violin plot
+  geom_boxplot(width = 0.1, fill = "white") + # Add boxplot inside
+  scale_fill_manual(values = plotcolors[5:6]) +
+  theme_minimal() +
+  labs(x = "Approved", y = "LTV Ratio") +
+  theme(legend.position = "none")
+
+plot_uria <- ggplot(data, aes(x = approved, y = uria, fill = approved)) +
+  geom_violin(trim = T) + # Add violin plot
+  geom_boxplot(width = 0.1, fill = "white") + # Add boxplot inside
+  scale_fill_manual(values = plotcolors[7:8]) +
+  theme_minimal() +
+  labs(x = "Approved", y = "Unempl. Rate in Appl. Industry") +
+  theme(legend.position = "none")
+
+boxplots <- (plot_hir | plot_odir | plot_lvr | plot_uria) + 
+  plot_layout(guides = 'collect') &
+  theme(legend.position = "bottom")
+
+# Continuous predictors, trivariate, self-employed
+plot_self_hir <- ggplot(data, aes(x = approved, y = hir, fill = self)) +
+  geom_violin(trim = T) +
+  scale_fill_manual(values = plotcolors[1:2]) +
+  theme_minimal() +
+  labs(x = "Approved", y = "Ratio of Housing Expenses to Income") +
+  theme(legend.position = "bottom")
+
+plot_self_odir <- ggplot(data, aes(x = approved, y = odir, fill = self)) +
+  geom_violin(trim = T) +
+  scale_fill_manual(values = plotcolors[1:2]) +
+  theme_minimal() +
+  labs(x = "Approved", y = "Ratio of other Debt Payments to Income") +
+  theme(legend.position = "bottom")
+
+plot_self_lvr <- ggplot(data, aes(x = approved, y = lvr, fill = self)) +
+  geom_violin(trim = T) +
+  scale_fill_manual(values = plotcolors[1:2]) +
+  theme_minimal() +
+  labs(x = "Approved", y = "LTV Ratio") +
+  theme(legend.position = "bottom")
+
+plot_self_uria <- ggplot(data, aes(x = approved, y = uria, fill = self)) +
+  geom_violin(trim = T) +
+  scale_fill_manual(values = plotcolors[1:2]) +
+  theme_minimal() +
+  labs(x = "Approved", y = "Unempl. Rate in Appl. Industry") +
+  theme(legend.position = "bottom")
+
+violinplots <- (plot_self_hir | plot_self_odir | plot_self_lvr | plot_self_uria) + 
+  plot_layout(guides = 'collect') &
+  theme(legend.position = "bottom")
+
+# Factor Predictors (mcs, self, single, white)
+plot_mcs <- ggplot(data, aes(x = approved, fill = mcs)) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_manual(values = plotcolors[1:4]) +
+  theme_minimal() +
+  labs(x = "Approved", y = "Percentage") + 
+  theme(legend.position = "bottom")
+
+plot_self <- ggplot(data, aes(x = approved, fill = self)) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_manual(values = plotcolors[5:6]) +
+  theme_minimal() +
+  labs(x = "Approved", y = "Percentage") + 
+  theme(legend.position = "bottom")
+
+plot_single <- ggplot(data, aes(x = approved, fill = single)) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_manual(values = plotcolors[7:8]) +
+  theme_minimal() +
+  labs(x = "Approved", y = "Percentage") + 
+  theme(legend.position = "bottom")
+
+plot_white <- ggplot(data, aes(x = approved, fill = white)) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_manual(values = plotcolors[9:10]) +
+  theme_minimal() +
+  labs(x = "Approved", y = "Percentage") + 
+  theme(legend.position = "bottom")
+
+mosaicplots <- (plot_mcs | plot_self | plot_single | plot_white)
+
+plot_lowdebt <- ggplot(data, aes(x = low_debt, fill = approved)) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_manual(values = plotcolors[9:10]) +
+  theme_minimal() +
+  labs(x = "Approved", y = "Percentage") + 
+  theme(legend.position = "bottom")
+
+# Fit full model
+baseline <- glm(approved ~ ., data = data, family = binomial(link = "logit"))
+
+### Experiments
+data_new <- data %>%
+  mutate(low_debt = if_else(odir < 0.2, 1, 0)) %>% 
+  dplyr::select(!odir)
+
+
+baseline_new <- glm(approved ~ ., data = data_new, family = binomial(link = "logit"))
+
+### Export ###
+# tables
+setwd(tab)
+print.xtable(sumtable, type = "latex", file = "sumstats.tex", 
+             include.rownames = TRUE, digits = 2, align = c("l", rep("c", 4)),
+             label = "tab1", caption.placement = "top", 
+             floating = T, table.placement = "H")
+
+print(xtable(baseline, caption = "Baseline Model"), 
+      file = "baseline.tex", digits = 3, label = "baseline",
+      caption.placement = "top", table.placement = "H", floating = T)
+setwd(root)
+
+# figures
+setwd(fig)
+ggsave(plot = boxplots, "boxplots.png")
+ggsave(plot = mosaicplots, "mosaicplots.png")
+ggsave(plot = violinplots, "violinplots.png")
+setwd(root)
+
+
+
